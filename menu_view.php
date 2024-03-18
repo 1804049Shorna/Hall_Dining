6944a8b5f4b4e390ee953939c4538b90eaa737e4
Shanjida Nahid Shorna <sanjidanahid53@gmail.com>
2024-03-18 10:29:42 +0600

    Changes

--- menu_view.php
+++ menu_view.php
@@ -26,7 +26,7 @@
 		}
 	</style>
 </head>
-<body style ="background-color:#D3D3D3 ">
+<body style ="background-color: white ">
      <a href="index.php">&#8592;</a>
      <?php 
           $sql = "SELECT menu FROM images2 ORDER BY SerialNo DESC";
@@ -35,7 +35,7 @@
           if (mysqli_num_rows($res) > 0) {
           	while ($images = mysqli_fetch_assoc($res)) {  ?>
              
-             <div class="alb" style="height: 100px; width=80px ; border: 2px solid black; background-color : white; margin:5px ">
+             <div class="alb" style="height: 100px; width=80px ; border: 1px solid black; background-color : white; margin:5px ">
 			 	<?php echo $images['menu'] ?>
              </div>
           		
